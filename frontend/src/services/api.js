bb9e784b449b5c7819f840e46c6adb3fac670d15
BheemanapalleSreevani <sreevani940@gmail.com>
2026-05-07 14:22:41 +0530

    Update API base URL for task manager service

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://taskmanager-ecwq.onrender.com/',
+   baseURL: 'https://taskmanager-umpg.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
